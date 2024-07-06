045c6ab42e2639fab6608b19d171fb1931d57844
Alvaro Nicolas Ortiz <160544793+ANicolasOrtiz@users.noreply.github.com>
2024-07-06 16:26:25 +0000

    ejercicio terminado

--- src/app.py
+++ src/app.py
@@ -15,6 +15,15 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+initial_members = [
+    {"first_name": "John", "age": 33, "lucky_numbers": [7, 13, 22]},
+    {"first_name": "Jane", "age": 35, "lucky_numbers": [10, 14, 3]},
+    {"first_name": "Jimmy", "age": 5, "lucky_numbers": [1]}
+]
+
+for member in initial_members:
+    jackson_family.add_member(member)
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -26,17 +35,35 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+     member = jackson_family.get_member(member_id)
+     if member:
+        return jsonify(member), 200
+     else:
+        return jsonify({"message":"Member not found"}), 400
 
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def add_member():
+    body =  request.get_json()
+    if not body:
+        return jsonify({"message": "Invalid Request"}), 400
+    
+    jackson_family.add_member(body)
+    return jsonify({"message": "Member added successfully"}), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        jackson_family.delete_member(member_id)
+        return jsonify({"done" : True}), 200
+    else:
+        return jsonify({"message" : "Member not found"}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
